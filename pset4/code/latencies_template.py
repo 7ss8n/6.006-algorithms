e9c6b63558eb1c3ddefcb9fab84c2c0beb78c35b
Milo Knowles <mknowles@mit.edu>
2016-11-15 17:25:06 -0500

    Now keeping track of num best paths

--- pset4/code/latencies_template.py
+++ pset4/code/latencies_template.py
@@ -5,6 +5,8 @@
 # PART A: Fill in the code for part a
 # 
 
+from collections import deque
+
 def printMatrix3D(m):
     for row in m:
         print(row)
@@ -76,8 +78,12 @@ def conservative_latencies(N, L):
     #initialize the second-best matrix B to start out with infinity for every distance
     B = [[[float("inf") for x in range(N)] for y in range(N)] for z in range(N+1)]
     
-    # parent[i][j] is the node directly before j in the path from i to j
+    # parent[i][j] is a list of nodes that could be directly before j in the path from i to j
+    # we have a list in place of a single number to allow for multiple shortest paths
     parent = [[0 for x in range(N)] for y in range(N)]
+
+    #create one last array to keep track of when there are multiple best paths
+    numBestPathsArray = [[1 for x in range(N)] for y in range(N)]
 
     #NOTE: order of indexing is z, x, y
     # intialize A with the weights of edges that we know
@@ -108,8 +114,15 @@ def conservative_latencies(N, L):
                     # Now update the shortest path in A
                     A[0][u][v] = A[0][u][k]+A[0][k][v]
 
-                    #update the parent array
+                    # now the node right before v in the best path from u->v is the node right before v in the best path from k->v
                     parent[u][v] = parent[k][v]
+
+                    #since the path was improved, there is now only ONE shortest path from u->v
+                    numBestPathsArray[u][v] = 1
+
+                # if the new path we are considering has the SAME length as our shortest path so far, increment the number of shortest paths
+                elif (A[0][u][k]+A[0][k][v] == A[0][u][v] and (k != u and k != v)):
+                    numBestPathsArray[u][v] += 1
 
                 #If k is not equal to u or v, consider 2 cases where the second shortest path could be improved using intermediate node k
                 if (k != u and k != v):
@@ -121,22 +134,29 @@ def conservative_latencies(N, L):
                     if A[0][u][k]+B[0][k][v] < B[0][u][v]:
                         B[0][u][v] = A[0][u][k]+B[0][k][v]
 
+    print("Num best paths")
+    print(numBestPathsArray)
+
+
     # now build the shortest paths from each u to v
     shortestPathDict = {}
     for u in range(N):
         for v in range(N):
-            path = []
             start = u
             end = v
+
+            #path is a deque to allow fast appending to left
+            path = deque()
             while end != start:
-                path.append(parent[start][end])
+                path.appendleft(end)
                 end = parent[start][end]
+            path.appendleft(start)
             shortestPathDict[(u,v)] = path
 
-    print(shortestPathDict)
+    # print("Shortest path dict")
+    # print(shortestPathDict)
     #print(parent)
     #printMatrix3D(B[0])
-
 
     # now determine the shortest nontrivial cycle around each vertex in the graph
     shortestCycleDict = {}
@@ -151,13 +171,18 @@ def conservative_latencies(N, L):
         # add the shortest cycle to the dictionary
         shortestCycleDict[startVertex] = bestCycleLength
 
-    print("Cycle dict")
-    print(shortestCycleDict)
+    #print("Cycle dict")
+    #print(shortestCycleDict)
     #For each vertex in every shortest path in A, see if adding a single nontrivial cycle will improve the second best path in B
     for i,j in shortestPathDict:
-        shortestPath = shortestPathDict[(i,j)] # a sequence of vertices
+        
+        # see if there are multiple shortest paths from i to j, in which case B should also contain the shortest path length
+        if numBestPathsArray[i][j] > 1:
+            B[0][i][j] = A[0][i][j]
+            continue # the value in B can't get any smaller if this is the case
 
         # see if the shortest path plus a cycle at some vertex v improves our SBP
+        shortestPath = shortestPathDict[(i,j)] # a sequence of vertices
         for v in shortestPath:
             if A[0][i][j] + shortestCycleDict[v] < B[0][i][j]:
                 B[0][i][j] = A[0][i][j] + shortestCycleDict[v]
